4a2bc6be83742656afc7d7657778147059c676b9
Joy <jtkaufman737@gmail.com>
2018-05-11 19:23:32 -0400

    Adding first view

---
+++ knock/knock/knockapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import include,path
+
+from . import views
+
+urlpatterns = [
+   path('jokes/',include('jokes.urls')),
+   path('admin/',admin.site.urls),
+]
