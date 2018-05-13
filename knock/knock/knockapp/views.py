edfc98917234a1ead1a3c3e70008e5bbf70f94c7
Joy <jtkaufman737@gmail.com>
2018-05-13 14:21:46 -0400

    Create view

--- knock/knock/knockapp/views.py
+++ knock/knock/knockapp/views.py
@@ -5,10 +5,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world, you are at the startapp index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." %question_id)
+    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s")
+    response = "You're looking at the results of question %s"
     return HttpResponse(response % question_id)
