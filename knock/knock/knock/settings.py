018bf7e7cef6dcee74f553485e64ae962e10a8ad
Joy <jtkaufman737@gmail.com>
2018-05-13 10:04:04 -0400

    Add migrations and models

--- knock/knock/knock/settings.py
+++ knock/knock/knock/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'knockapp.apps.KnockappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- knock/knock/knockapp/models.py
+++ knock/knock/knockapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=500)
+    pub_date = models.DateTimeField('date published')
+
+class Punchline(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # Remember this from Ruby, destroying child db object
+    punchline_text = models.CharField(max_length=500)
