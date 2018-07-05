cbd8ecc77a924371143fa243600dcf5698821b99
Joy <jtkaufman737@gmail.com>
2018-07-04 21:00:33 -0400

    Move up to gh to delete local

--- knock/knock/knockapp/views.py
+++ knock/knock/knockapp/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import Question
+from .models import Question
+from django.http import Http404
 
 def index(request):
-    return HttpResponse("Hello world, you are at the startapp index")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('knockapp/index.html')
+    context = {
+        'latest_question_list':latest_question_list,
+    }
+    return HttpResponse(template.render(context,request))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Joke doesn't exist")
+    return render(request, 'knockapp/detail.html',{'question':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s"
